565b09b6b0b5ca60c90227d7ace3ba1ee0d30dc4
tdelmasiauto <t.delmas@itautomotive.fr>
2022-12-07 19:35:19 +0100

    Add AdminUser && Authenticator for __secured_admin__ && create ContactType form

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\AdminUserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\AdminUser;
 
 class TestController extends AbstractController
 {
     #[Route('/test')]
-    public function index(): Response
+    public function index(UserPasswordHasherInterface $passwordHasher , AdminUserRepository $adminUserRepository): Response
     {
+        $user = new AdminUser();
+        $plaintextPassword = 'mhm36Ek6NU2E';
+
+        $user->setUsername('MarieDo');
+        $user->setRoles(['ROLE_ADMIN']);
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        var_dump($user);
+
+//        $adminUserRepository->save($user, true);
+
         return new Response('testController here 2');
     }
 
