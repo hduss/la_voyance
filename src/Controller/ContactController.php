d99c5caf14b9afe8ab082bb60bdf521b269c9e0d
tdelmasiauto <t.delmas@itautomotive.fr>
2022-12-07 20:27:21 +0100

    Add ContactType and ContactController && Add PUREcss

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $form = $this->createForm(ContactType::class);
+//        var_dump($form);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +15,16 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'required' => true,
+            ])
+            ->add('content', TextareaType::class, [
+                'required' => true
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer moi un message'
+            ])
         ;
     }
 
