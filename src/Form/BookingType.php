8ca85628c4daf2d8846937693fbdd4d847394e4d
tdelmasiauto <t.delmas@itautomotive.fr>
2022-12-16 00:01:19 +0100

    Improvement BookingType render in template

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Reservation;
-use PharIo\Manifest\Email;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -12,31 +11,58 @@ use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 
 class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+        $now = new \DateTime('now');
         $builder
             ->add('name', TextType::class, [
-                'required' => true
+                'required' => true,
+                'label' => 'Prénom',
+                'constraints' => [new NotBlank()]
             ])
             ->add('lastname', TextType::class, [
-                'required' => true
+                'required' => true,
+                'label' => 'Nom de Famille',
+                'constraints' => [new NotBlank()]
+
             ])
             ->add('email', EmailType::class, [
-                'required' => true
+                'required' => true,
+                'label' => 'Adresse mail',
+                'constraints' => [
+                    new NotBlank(),
+                    new Email([
+                    'mode' => 'strict',
+                ])]
             ])
             ->add('phone', TelType::class, [
-                'required' => true
+                'required' => true,
+                'label' => 'Numéro de téléphone'
             ])
             ->add('date', DateTimeType::class, [
-                'widget' => 'single_text',
+                'data' => $now,
+                'attr' => ['min' => $now],
+                'constraints' => [
+                    new NotBlank(),
+                    new Range(['max' => 'today'])
+                ],
+                'date_widget' => 'single_text',
+                'date_label' => 'Date',
+                'time_widget' => 'single_text',
+                'time_label' => 'Heure',
                 'view_timezone' => 'Europe/Paris',
                 'required' => true
             ])
 //            ->add('prestation')
             ->add('submit', SubmitType::class, [
+                'label' => 'Réserver la consultation'
 //                'mapped' => false
             ])
         ;
